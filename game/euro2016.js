59ac2d7122855cf4ee08f7391121c03a21e85d3f
André Ferreira <220001208@esg.ipsantarem.pt>
2025-04-01 14:32:14 +0100

    Adição e correção de comentários

--- game/euro2016.js
+++ game/euro2016.js
@@ -4,7 +4,7 @@
  *
  */
 
-const game = {}; // encapsula a informação de jogo. Está vazio mas vai-se preenchendo com definições adicionais.
+const game = {}; // encapsula a informação do jogo. Está vazio mas será preenchido com definições adicionais.
 
 // sons do jogo
 const sounds = {
@@ -15,16 +15,16 @@ const sounds = {
   win: "",
 };
 
-// numero de linhas e colunas do tabuleiro;
+// número de linhas e colunas do tabuleiro
 const ROWS = 4;
 const COLS = 4;
 
-game.sounds = sounds; // Adicionar os sons sons do jogo ao objeto game.
+game.sounds = sounds; // Adiciona os sons do jogo ao objeto game.
 game.board = Array(COLS)
   .fill()
   .map(() => Array(ROWS)); // criação do tabuleiro como um array de 6 linhas x 8 colunas
 
-// Representa a imagem de uma carta de um país. Esta definição é apenas um modelo para outros objectos que sejam criados
+// Representa a imagem de uma carta de um país. Esta definição é apenas um modelo para outros objetos que sejam criados
 // com esta base através de let umaFace = Object.create(face).
 const face = {
   country: -1,
@@ -33,27 +33,27 @@ const face = {
 };
 
 const CARDSIZE = 102; // tamanho da carta (altura e largura)
-let faces = []; // Array que armazena objectos face que contêm posicionamentos da imagem e códigos dos paises
+let faces = []; // Array que armazena objetos face que contêm posicionamentos da imagem e códigos dos países
 
 window.addEventListener("load", init, false);
 
 function init() {
   game.stage = document.querySelector("#stage");
-  setupAudio(); // configurar o audio
+  setupAudio(); // configurar o áudio
   getFaces(); // calcular as faces e guardar no array faces
   createCountries(); // criar países
   tempo(); // iniciar o temporizador
   sounds.background.play();
 }
 
-// Cria os paises e coloca-os no tabuleiro de jogo(array board[][])
+// Cria os países e coloca-os no tabuleiro de jogo (array board[][])
 function createCountries() {
-  let indices = [...Array(8).keys(), ...Array(8).keys()]; // Cria um array 8 elementos
-  indices.sort(() => Math.random() - 0.5); // Baralha os indices
+  let indices = [...Array(8).keys(), ...Array(8).keys()]; // Cria um array com 8 elementos duplicados
+  indices.sort(() => Math.random() - 0.5); // Baralha os índices
 
   for (let i = 0; i < 16; i++) {
     let umaCarta = document.createElement("div");
-    umaCarta.classList.add("carta", "escondida");
+    umaCarta.classList.add("carta", "escondida"); // Aplica as classes "carta" e "escondida" do CSS
 
     // Posição da carta com base nas coordenadas das faces
     umaCarta.style.backgroundPositionX = faces[indices[i]].x;
@@ -117,7 +117,7 @@ function flipCard(card) {
 // Verifica se encontrou o par
 function checkMatch(cards) {
   if (cards[0].dataset.country === cards[1].dataset.country) {
-    cards.forEach((card) => card.classList.add("encontrada")); // Marca as cartas como encontradas
+    cards.forEach((card) => card.classList.add("encontrada", "matched")); // Marca as cartas como encontradas e aplica a animação
     game.sounds.success.play();
     flipped = []; // Reseta as cartas viradas
     checkForWin();
@@ -126,11 +126,11 @@ function checkMatch(cards) {
       cards.forEach((card) => card.classList.add("escondida")); // Volta a virar as cartas
       game.sounds.hide.play();
       flipped = []; // Reseta as cartas viradas
-    }, 500); // Deixa as cartas vísiveis por 500ms antes de voltar a esconder
+    }, 500); // Deixa as cartas visíveis por 500ms antes de voltar a esconder
   }
 }
 
-// Verificar se o jogador ganhou
+// Verifica se o jogador ganhou
 function checkForWin() {
   let allCards = Array.from(document.querySelectorAll(".carta"));
   let unmatchedCards = allCards.filter(
@@ -172,7 +172,7 @@ function tempo() {
   let contador = 0;
   let maxCount = 45;
 
-  // Apagar qualquer timer anterior
+  // Apagar qualquer temporizador anterior
   if (timeHandler) {
     clearInterval(timeHandler);
   }
@@ -182,15 +182,15 @@ function tempo() {
     document.getElementById("time").value = contador;
 
     if (contador === maxCount - 5) {
-      document.getElementById("time").classList.add("warning");
+      document.getElementById("time").classList.add("warning"); // Aplica a classe "warning" para animar a barra de progresso
     }
 
     if (contador === maxCount) {
       clearInterval(timeHandler);
       document.getElementById("time").classList.remove("warning");
       scramble(); // Baralha as cartas novamente
-      tempo(); // Restart the timer
-      contador = 0; // Reset the contador
+      tempo(); // Reinicia o temporizador
+      contador = 0; // Reseta o contador
     }
   }, 1000);
 }
@@ -199,7 +199,7 @@ function tempo() {
  ** /!\ NÃO MODIFICAR ESTAS FUNÇÕES /!\
 -------------------------------------------------------------------------------------------------- */
 
-// configuração do audio
+// configuração do áudio
 function setupAudio() {
   game.sounds.background = document.querySelector("#backgroundSnd");
   game.sounds.success = document.querySelector("#successSnd");
@@ -207,23 +207,23 @@ function setupAudio() {
   game.sounds.hide = document.querySelector("#hideSnd");
   game.sounds.win = document.querySelector("#goalSnd");
 
-  // definições de volume;
+  // definições de volume
   game.sounds.background.volume = 0.05; // o volume varia entre 0 e 1
 
   // nesta pode-se mexer se for necessário acrescentar ou configurar mais sons
 }
 
-// calcula as coordenadas das imagens da selecao de cada país e atribui um código único
+// calcula as coordenadas das imagens da seleção de cada país e atribui um código único
 function getFaces() {
-  /* NÂO MOFIFICAR ESTA FUNCAO */
+  /* NÃO MODIFICAR ESTA FUNÇÃO */
   let offsetX = 1;
   let offsetY = 1;
   for (let i = 0; i < 3; i++) {
     offsetX = 1;
     for (let j = 0; j < 3; j++) {
       let countryFace = Object.create(face); // criar um objeto com base no objeto face
-      countryFace.x = -(j * CARDSIZE + offsetX) + "px"; // calculo da coordenada x na imagem
-      countryFace.y = -(i * CARDSIZE + offsetY) + "px"; // calculo da coordenada y na imagem
+      countryFace.x = -(j * CARDSIZE + offsetX) + "px"; // cálculo da coordenada x na imagem
+      countryFace.y = -(i * CARDSIZE + offsetY) + "px"; // cálculo da coordenada y na imagem
       countryFace.country = "" + i + "" + j; // criação do código do país
       faces.push(countryFace); // guardar o objeto no array de faces
       offsetX += 2;
